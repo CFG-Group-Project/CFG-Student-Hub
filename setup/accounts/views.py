b2b605e32779b4432556afa8538531005ceee587
Ayisha Alli <ayisha.alli@playtech.com>
2022-07-22 21:56:33 +0100

    accounts setup

---
+++ setup/accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import NewUserForm
+
+
+#from .forms import NewUserForm
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+def register(request):
+    form = NewUserForm()
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'pages/register.html', context)
+
+
+def login(request):
+    return render(request, 'pages/login.html')
