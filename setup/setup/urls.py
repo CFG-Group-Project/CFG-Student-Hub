9184ad77c8d16e97ef198ad2cfe1f1d88f3ae136
Ayisha Alli <ayisha.alli@playtech.com>
2022-07-22 21:56:14 +0100

    added urls for login and register

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('register/', include('register.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
