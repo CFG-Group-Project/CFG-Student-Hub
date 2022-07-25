2ddece46a4d606faedc0205ac643eb8882dcc42b
Amina Codes <aminaahmedabc@gmail.com>
2022-07-25 22:24:06 +0100

    added forum template

--- setup/forum/views.py
+++ setup/forum/views.py
@@ -4,3 +4,12 @@ from . import views
 # Create your views here.
 def forum(request):
     return render(request, 'forum/forum.html')
+
+
+def discussion(request):
+    return render(request, 'forum/discussion.html')
+
+
+def create_post(request):
+    return render(request, 'forum/create_post.html')
+
--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cfgdatabase',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
