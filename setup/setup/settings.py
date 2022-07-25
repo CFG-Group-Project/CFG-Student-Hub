164222c1e74cda2eedbf169ecc34a49e26d79eae
Ayisha Alli <ayisha.alli@playtech.com>
2022-07-25 18:51:51 +0100

    password change to root

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cfgdatabase',
         'USER': 'root',
-        'PASSWORD': 'tTyq178fighter2',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
