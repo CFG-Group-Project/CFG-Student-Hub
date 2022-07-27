fbb7d93f102f2606399bd9a3969921bfe5d2b6d4
Amina Codes <aminaahmedabc@gmail.com>
2022-07-27 20:21:47 +0100

    changes to timer app

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -7,6 +7,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cfgdatabase',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -119,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- setup/studyzone/views.py
+++ setup/studyzone/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import timeritem
 # Create your views here.
 
 
 def study(request):
     return render(request, 'studyzone/study.html')
+    # return render(request, '/study.html')
+
