2a26c6c20fca3c92a184a0a09141b60724e605d5
vixbc <92274245+vixbc@users.noreply.github.com>
2022-07-29 07:35:29 +0100

    amends

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', include('contact.urls')),
     path('', include('studyzone.urls')),
     path('', include('dashboard.urls')),
-    path('', include('resources.urls'))
+    path('', include('resources.urls')),
     path('', include('accounts.urls')),
     path('', include('django.contrib.auth.urls')),  # Authentication URL's - Built In
     path('admin/', admin.site.urls),
