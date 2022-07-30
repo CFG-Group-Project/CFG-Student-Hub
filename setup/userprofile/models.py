7a2bae4293691d3c41afc4837b90280d86552e0c
Ayisha Alli <ayisha.alli@playtech.com>
2022-07-30 18:40:44 +0100

    .

--- setup/userprofile/models.py
+++ setup/userprofile/models.py
@@ -27,7 +27,6 @@ class Profile(models.Model):
         verbose_name_plural = _('Profiles')
 
 
-
 class Category(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
