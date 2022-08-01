a00e13ce0e85c27f7bbf3830f80e996820bfdb74
vixbc <92274245+vixbc@users.noreply.github.com>
2022-08-01 20:28:17 +0100

    .

--- setup/resources/urls.py
+++ setup/resources/urls.py
@@ -5,14 +5,14 @@ app_name = 'resources'
 
 urlpatterns = [
     path('resources/', views.resources, name='resources'),
-path('resources/programs', views.programs, name='programs'),
+    path('resources/programs', views.programs, name='programs'),
     path('resources/mynotes',views.usernotes,name='user-sub'),
     path('resources/mynotes/delete_note/<int:pk>',views.delete_note,name='delete_note'),
     path('resources/mynotes/<int:pk>',views.NotesDetailView.as_view(),name='notes-detail'),
     path('resources/program/<name>', views.ClassPage, name='class-page'),
     path('resources/submit', views.submit, name='submit'),
     path('resources/submit-thanks', views.submit_thanks, name='submit-thanks'),
-    path('resources/lesson/<int:pk>', views.LessonDetailView.as_view(), name='material-page'),
+    path('resources/lesson/<int:pk>', views.LessonDetailView.as_view(), name='lesson-detail'),
     # path('resources/', KnowledgeBank.as_view(),name='material'),
     # path('resources/<Program_name>', ProgBank.as_view(),name='program'),
     # path('resources/<int:bank_id>', views.materials, name='materials'),
