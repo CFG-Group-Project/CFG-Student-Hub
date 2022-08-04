4a0786a8d61236a42d7b374d61bb12366f44f7dc
vixbc <92274245+vixbc@users.noreply.github.com>
2022-08-04 17:36:27 +0100

    dash & notes search

--- setup/contact/models.py
+++ setup/contact/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    subbed = models.DateTimeField(auto_now=True)
+    resolved = models.BooleanField(editable=True,default=False)
+
+    class Meta:
+        verbose_name = "Contact Sub"
+        verbose_name_plural = "Contact Subs"
+
+    def __str__(self):
+        return str(self.subject) + ' (' + str(self.user) +')'
--- setup/resources/forms.py
+++ setup/resources/forms.py
@@ -4,24 +4,24 @@ from .models import *
 
 
 class CreateNewResource(ModelForm):
-    lesson = forms.CharField(label='Lesson Title', max_length=50)
-    topic = forms.CharField(label='Topic', max_length=50)
+    lesson = forms.CharField(label='Lesson Title', max_length=50, )
+    topics = forms.CharField(label='Topic', max_length=50, )
     week = forms.CharField(label='Week', max_length=3)
     lesson_slide = forms.URLField(label='Lesson Slides', max_length=200)
-    code = forms.FileField(label='Lesson Code File', max_length=None, allow_empty_file=True)
-    show = forms.BooleanField(label='Is this ready for study view?',initial=False,required=False)
-    rectutorial = forms.URLField(label='Recommended Tutorial', max_length=200,required=False)
-
+    code = forms.CharField(label='Code File Link', max_length=400, required=False)
+    show = forms.BooleanField(label='Is this ready for students to view?', initial=False, required=False)
+    rectutorial = forms.URLField(label='Recommended Tutorial', max_length=200, required=False)
+    program = forms.ModelChoiceField(Program.objects.all())
     class Meta:
         model = Material
-        fields = ['lesson', 'topic', 'week', 'code', 'show', 'rectutorial']
-
+        fields = ['lesson', 'topics', 'lesson_slide', 'week', 'code', 'rectutorial', 'program', 'show']
 
 
 class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
-        fields = ['title','content','link']
+        fields = ['title', 'content', 'link']
+
 
 class SearchForm(forms.Form):
-    text = forms.CharField(max_length=100,label='What would you like to search for?',required=False)
+    text = forms.CharField(max_length=100, label='What would you like to search for?', required=False)
