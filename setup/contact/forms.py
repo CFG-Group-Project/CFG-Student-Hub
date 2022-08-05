32d07b642532389caf908f4139a3d796c72afc5d
vixbc <92274245+vixbc@users.noreply.github.com>
2022-08-05 06:29:11 +0100

    tidying up

--- setup/contact/forms.py
+++ setup/contact/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm
 from .models import *
 
 class ContactForm(forms.ModelForm):
+
     class Meta:
         model = ContactUs
         fields = ['subject', 'content']
