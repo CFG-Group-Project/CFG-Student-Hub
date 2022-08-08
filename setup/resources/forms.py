eb96d9f7cbd36e86b23f3c7ddf3f3a2d3d66b700
vixbc <92274245+vixbc@users.noreply.github.com>
2022-08-08 14:21:15 +0100

    admin dash, user notes complete

--- setup/resources/forms.py
+++ setup/resources/forms.py
@@ -3,22 +3,6 @@ from django.forms import ModelForm
 from .models import *
 
 
-class CreateNewResource(ModelForm):
-    lesson = forms.CharField(label='Lesson Title', max_length=50, )
-    topics = forms.CharField(label='Topic', max_length=50, )
-    week = forms.CharField(label='Week', max_length=3)
-    lesson_slide = forms.URLField(label='Lesson Slides', max_length=200,widget= forms.TextInput
-                           (attrs={'placeholder':'Publish your Google Slide and then copy the link in the first set '
-                                                 'of quotation marks ONLY'}))
-    code = forms.CharField(label='Code File Link', max_length=400, required=False)
-    show = forms.BooleanField(label='Is this ready for students to view?', initial=False, required=False)
-    rectutorial = forms.URLField(label='Recommended Tutorial', max_length=200, required=False)
-    program = forms.ModelChoiceField(Program.objects.all())
-    class Meta:
-        model = Material
-        fields = ['lesson', 'topics', 'lesson_slide', 'week', 'code', 'rectutorial', 'program', 'show']
-
-
 class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
@@ -27,3 +11,9 @@ class NotesForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     text = forms.CharField(max_length=100, label='What would you like to search for?', required=False)
+
+
+class MakeVisible(forms.ModelForm):
+    class Meta:
+        model = Material
+        fields = ['show']
