815f4ced9df82967b94f09b8eaf0a5780bc7216b
Amina Codes <aminaahmedabc@gmail.com>
2022-08-08 20:55:33 +0100

    fixed exit button positioning

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cfgdatabase',
         'USER': 'root',
-        'PASSWORD': 'fahad350',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
