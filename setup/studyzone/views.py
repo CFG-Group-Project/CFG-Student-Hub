8a41314637d6a3e672fad19a9a7224b5eec80e00
Amina Codes <aminaahmedabc@gmail.com>
2022-08-08 20:53:53 +0100

    added api to studyzone

--- setup/studyzone/views.py
+++ setup/studyzone/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import timeritem
 # Create your views here.
+import requests
 
+#
+# def study(request):
+#     return render(request, 'studyzone/study.html')
+#     # return render(request, '/study.html')
 
 def study(request):
-    return render(request, 'studyzone/study.html')
-    # return render(request, '/study.html')
+    url = "https://motivational-quotes1.p.rapidapi.com/motivation"
 
+    payload = {
+        "key1": "value",
+        "key2": "value"
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "7131b4b784mshfcc1203f2832389p1f0b8cjsn7f45824084c1",
+        "X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
+    }
+
+    quotes = requests.request("POST", url, json=payload, headers=headers)
+
+    print(quotes.text)
+    return render(request, "studyzone/study.html", {'quotes': quotes})
